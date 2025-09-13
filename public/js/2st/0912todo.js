fb73b9eb0d982a624db4d5503b63cc5a7651429e
itomato1369 <itomato1369@gmail.com>
2025-09-13 13:44:32 +0900

    0913주말공부

--- public/js/2st/0912todo.js
+++ public/js/2st/0912todo.js
@@ -4,10 +4,23 @@
 // 계산하는 기능 구현 사칙연산
 // 함수 이름 정의 plus, minus, multiply, divide
 //
-//
+// document.querySelector('#') 은 #앞에 붙은 주소를 찾는 것
+// const a = b    a값이 b 로 고정
 
-   let heading = document.querySelector("#heading");
-        heading.onclick = function() {
-            heading.style.color = "red";
-        }
 
+function plus(num1, num2) {
+    return num1 + num2;
+}
+function minus(num1, num2) {
+    return num1 - num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+function divide(num1, num2) {     // 나누기할 때 왜 0 / 2 는 되는데, 2 / 0 은 안되는지
+    if (num2 == 0) {
+        return "0으로 나눌 수 없습니다";
+    } else {
+        return num1 / num2;
+    }
+} 
--- public/js/2st/review.js
+++ public/js/2st/review.js
@@ -1,9 +1,27 @@
-//inch * 2.54 연산식은 식 (expression)
-//"공부해야돼" 문자열도 식
-//530  숫자도 식
+// inch * 2.54 연산식은 식 (expression)
+// "공부해야돼" 문자열도 식
+// 530  숫자도 식
 // 문(statement)는 명령 명령의 끝은;
+// 변수를 지정 let 변수명
 // alert("과제를해");
 // let reply = confirm("과제를 했니?");
-let name = prompt("이름을 입력하세요");
-document.write("<b>"+ name + "</b>님 안녕하세요");
-
+// let name = prompt("이름을 입력하세요");
+// // document.write("<b>"+ name + "</b>님 안녕하세요");
+// console.log(name + "님 환영합니다");
+// let width;
+// let height;
+// width = 200;
+// height = 90;
+// let area = width * height;
+// console.log(area);
+// const 는 프로그램 안에서 값을 변경할 수 없다. 원주율 원의 넓이
+// const pie = 3.14;
+// let radius = prompt("반지름을 입력하세요");
+// let area = pie * radius * radius;
+// console.log(area);
+let Number = parseInt(prompt("숫자를 입력하세요"));
+if (Number % 3 == 0) {
+    console.log("3의 배수입니다")
+} else {
+    console.log("3의 배수가 아니다")
+}
