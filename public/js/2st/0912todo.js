41058f419939c8904f8a3285df0e9f85ff853b7c
itomato1369 <itomato1369@gmail.com>
2025-09-14 09:11:51 +0900

    0914공부

--- public/js/2st/0912todo.js
+++ public/js/2st/0912todo.js
@@ -5,22 +5,23 @@
 // 함수 이름 정의 plus, minus, multiply, divide
 //
 // document.querySelector('#') 은 #앞에 붙은 주소를 찾는 것
-// const a = b    a값이 b 로 고정
 
 
-function plus(num1, num2) {
-    return num1 + num2;
-}
-function minus(num1, num2) {
-    return num1 - num2;
-}
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-function divide(num1, num2) {     // 나누기할 때 왜 0 / 2 는 되는데, 2 / 0 은 안되는지
-    if (num2 == 0) {
-        return "0으로 나눌 수 없습니다";
-    } else {
-        return num1 / num2;
+function calculate() {
+    let num1 = document.querySelector("#num1").value;
+    let num2 = document.querySelector("#num2").value;
+    let operation = document.querySelector("#operation").value;
+    let result = 0;
+    console.log(num1, num2, operation);
+    switch (operation) {
+        case 'plus':
+            result = num1 + num2; break;
+        case 'minus':
+            result = num1 - num2; break;
+        case 'multiply':
+            result = num1 * num2; break;
+        case 'divide':
+            result = num1 / num2; break;
     }
-} 
+    document.querySelector('#result').value = result;
+}
--- public/js/2st/review.js
+++ public/js/2st/review.js
@@ -3,22 +3,27 @@
 // 530  숫자도 식
 // 문(statement)는 명령 명령의 끝은;
 // 변수를 지정 let 변수명
+
 // alert("과제를해");
 // let reply = confirm("과제를 했니?");
 // let name = prompt("이름을 입력하세요");
 // // document.write("<b>"+ name + "</b>님 안녕하세요");
 // console.log(name + "님 환영합니다");
+
 // let width;
 // let height;
 // width = 200;
 // height = 90;
 // let area = width * height;
 // console.log(area);
+
 // const 는 프로그램 안에서 값을 변경할 수 없다. 원주율 원의 넓이
+// const a = b    a값이 b 로 고정
 // const pie = 3.14;
 // let radius = prompt("반지름을 입력하세요");
 // let area = pie * radius * radius;
 // console.log(area);
+
 // let Number = parseInt(prompt("숫자를 입력하세요"));
 // if (Number % 3 == 0) {
 //     console.log("3의 배수입니다")
@@ -26,11 +31,12 @@
 //     console.log("3의 배수가 아니다")
 // }
 // for (초깃값; 조건; 증가식) {}
+
 let sum = 0;
-for (i = 1; i <6; i++) {
+for (let i = 1; i<=10000135326262727270; i++) {
     sum += i;
 }
-console.log(`1부터 5까지 더하면 ${sum}`);
+document.writeln(`${sum}`);
 // i 는 iterator 반복자
 
 let name = "Kim";
