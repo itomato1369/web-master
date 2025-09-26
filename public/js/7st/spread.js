631ff494c998e9a9ea6a87619550309889279241
doowoo <itomato1369@gmail.com>
2025-09-26 18:01:09 +0900

    0926

--- public/js/7st/spread.js
+++ public/js/7st/spread.js
@@ -14,30 +14,30 @@ const newVeggie = veggie;  // 객체의 주소 동일
 
 
 newVeggie.push('peas');
-console.log(veggie, newVeggie); // 같은 배열을 참조.
+// console.log(veggie, newVeggie); // 같은 배열을 참조.
 const anotherVeggie = [...veggie,...['grape']]; //[] 새로운 배열
-veggie.push('peanuts');
+// veggie.push('peanuts');
 console.log(veggie, anotherVeggie);
 
 // 펼침연산자.
-function sum(...num) {
-    let result = 0;
-    for (let n of num) {
-        result += n;
-    }
-    return result;
-};
-console.log(sum(1,2,3,4,5,6));
+// function sum(...num) {
+//     let result = 0;
+//     for (let n of num) {
+//         result += n;
+//     }
+//     return result;
+// };
+// console.log(sum(1,2,3,4,5,6));
 
-//
-const myFriend = {
-    name: 'Hong',
-    age: 20,
-};
-const yourFriend = myFriend; // heap 메모리의 주소 참조
-myFriend.age = 22;
+// //
+// const myFriend = {
+//     name: 'Hong',
+//     age: 20,
+// };
+// const yourFriend = myFriend; // heap 메모리의 주소 참조
+// myFriend.age = 22;
 
-const anFriend = {...myFriend}; // 새로운 객체를 생성 {}안의 값만 가져옴
-myFriend.name = 'Hwang';
+// const anFriend = {...myFriend}; // 새로운 객체를 생성 {}안의 값만 가져옴
+// myFriend.name = 'Hwang';
 
-console.log(myFriend, anFriend);
+// console.log(myFriend, anFriend);
